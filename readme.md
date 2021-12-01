6a0e8afc03ccb25ebeb1f3387a9dab8d8f5ce167
Pepijn de Vos <pepijndevos@gmail.com>
2021-12-01 12:29:32 +0100

    add setup and readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Pyttoresque
+
+A Python library for working with NyanCAD tools.
+
+`pyttoresque.netlist` is a module for downloading schematics from a Mosaic CouchDB and generating SPICE netlists.
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Pyttoresque",
+    author="Pepijn de Vos",
+    author_email="pepijndevos@gmail.com",
+    description="Library for working with NyanCAD tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NyanCAD/pyttoresque",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'ibmcloudant',
+        'pycapnp'
+        'pandas',
+    ],
+    python_requires='>=3.6',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+)
