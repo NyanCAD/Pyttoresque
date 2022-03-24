cefa0f0d17ab46e8040c5ed0ec075c0c54993d9d
Pepijn de Vos <pepijndevos@gmail.com>
2022-03-24 17:50:16 +0100

    update mosaic

--- pyttoresque/app/static/update.sh
+++ pyttoresque/app/static/update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl -o main.js https://nyancad.github.io/Mosaic/app/js/main.js
+curl -o common.js https://nyancad.github.io/Mosaic/app/js/common.js
+curl -o editor.js https://nyancad.github.io/Mosaic/app/js/editor.js
+curl -o libman.js https://nyancad.github.io/Mosaic/app/js/libman.js
 curl -o style.css https://nyancad.github.io/Mosaic/app/css/style.css
