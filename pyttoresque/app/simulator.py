61ef5da72e8a4937ee30b1bcccd88e3fe470df4b
Pepijn de Vos <pepijndevos@gmail.com>
2022-03-30 18:22:28 +0200

    somewat restored normality

--- pyttoresque/app/simulator.py
+++ pyttoresque/app/simulator.py
@@ -5,6 +5,8 @@ import panel as pn
 import param
 from pyttoresque import simserver, netlist, analysis
 
+pn.extension('plotly')
+
 class Configuration(param.Parameterized):
     schematic = param.String("top$top2")
     database_url = param.String("https://c6be5bcc-59a8-492d-91fd-59acc17fef02-bluemix.cloudantnosqldb.appdomain.cloud/schematics", label="Database URL")
@@ -48,18 +50,23 @@ class Simulation(param.Parameterized):
     vectors = param.List(precedence=0.5)
     rerun_on_change = param.Boolean(True, precedence=0.5)
 
+    def cmd(self, fs):
+        raise NotImplementedError()
+
+    def xlim(self, fs):
+        raise NotImplementedError()
 
 class TranSimulation(Simulation):
     maximum_timestep = param.Number(1e-5)
     start_time = param.Number(0)
     stop_time = param.Number(1e-3)
     
-    # @param.depends('maximum_timestep', 'start_time', 'stop_time', 'vectors')
+    @param.depends('maximum_timestep', 'start_time', 'stop_time', 'vectors')
     def cmd(self, fs):
-        return fs.commands.tran(self.maximum_timpestep, self.stop_time, self.start_time, self.vectors)
+        return fs.commands.tran(self.maximum_timestep, self.stop_time, self.start_time, self.vectors)
     
     @param.depends('start_time', 'stop_time')
-    def ylim(self):
+    def xlim(self):
         return (self.start_time, self.stop_time)
 
 
@@ -73,18 +80,19 @@ class AcSimulation(Simulation):
     start_frequency = param.Number(1)
     stop_frequency = param.Number(1e6)
     
-    # @param.depends('point_spacing', 'number_of_points', 'start_frequency', 'stop_frequency', 'vectors')
+    @param.depends('point_spacing', 'number_of_points', 'start_frequency', 'stop_frequency', 'vectors')
     def cmd(self, fs):
         return fs.commands.ac(self.point_spacing, self.number_of_points, self.start_frequency, self.stop_frequency, self.vectors)
     
     @param.depends('start_frequency', 'stop_frequency')
-    def ylim(self):
+    def xlim(self):
         return (self.start_frequency, self.stop_frequency)
 
 class OpSimulation(Simulation):
-    # @param.depends('vectors', watch=True, on_init=True)
+    @param.depends('vectors')
     def cmd(self, fs):
         return fs.commands.op(self.vectors)
+
 
 class SimTabs(param.Parameterized):
     sim = param.Selector([
@@ -105,10 +113,29 @@ class SimTabs(param.Parameterized):
 
 class Results(param.Parameterized):
     cmd = param.Action(label="Simulate")
+    xlim = param.Action()
+    data = param.Dict({})
 
+    def simulate(self, _):
+        for v in self.data.values():
+            v.clear()
+        res = self.cmd()
+        it = simserver.stream(res, self.data)
+        for _ in it:
+            self.param.trigger('data')
+            # print(self.data)
+
+    @param.depends('data')
     def panel(self):
-        print(self.cmd)
-        return pn.Pane(self.param.cmd)
+        btn = pn.widgets.Button(name="Simulate")
+        btn.on_click(self.simulate)
+        col = pn.Column(btn)
+        for k, v in self.data.items():
+            cols = analysis.active_traces(cols=list(v.data.columns))
+            if not v.data.empty:
+                plt = analysis.timeplot([v, cols]).opts(width=1000, height=500, xlim=self.xlim())
+                col.append(plt)
+        return col
 
 class Simulator(param.Parameterized):
     conf = param.ClassSelector(Configuration)
@@ -125,9 +152,10 @@ class Simulator(param.Parameterized):
     @param.depends('tabs.sim', watch=True, on_init=True)
     def _cmd(self):
         print(self.tabs.sim.cmd, self.conf.connect)
-        self.res.cmd = lambda _: self.tabs.sim.cmd(self.conf.connect()).wait()
+        self.res.cmd = lambda: self.tabs.sim.cmd(self.conf.connect())
+        self.res.xlim = self.tabs.sim.xlim
 
     def panel(self):
-        return pn.Row(self.conf, self.tabs.panel(), self.res.panel())
+        return pn.Row(self.conf, self.tabs.panel(), self.res.panel)
 
 Simulator().panel().servable()
