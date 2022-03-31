65bc6c80d89466e87ec0293346beaa938a952f7b
Pepijn de Vos <pepijndevos@gmail.com>
2022-03-31 16:40:51 +0200

    async simserver

--- pyttoresque/simserver.py
+++ pyttoresque/simserver.py
@@ -1,26 +1,52 @@
 from os import name
 from time import sleep
 from subprocess import Popen
+import asyncio
 import capnp
 from pyttoresque.api.Simulator_capnp import Ngspice, Xyce, Cxxrtl, AcType
 import numpy as np
 import pandas as pd
 from holoviews.streams import Buffer
 
+async def capnpreader(client, reader):
+    try:
+        while True:
+            data = await reader.read(4096)
+            client.write(data)
+    except Exception as e:
+        print(e)
 
-def connect(host, port=5923, simulator=Ngspice):
+
+async def capnpwriter(client, writer):
+    try:
+        while True:
+            data = await client.read(4096)
+            writer.write(data.tobytes())
+            await writer.drain()
+    except Exception as e:
+        print(e)
+
+
+async def connect(host, port=5923, simulator=Ngspice, autostart=True):
     """
     Connect to a simulation server at the given `host:port`,
     which should be a `simulator` such as `Ngspice` or `Xyce`.
 
     If `host` is set to "localhost" and no server is running,
-    we will attempt to start one automatically.
+    we will attempt to start one automatically,
+    unless `autostart=False`.
     """
     try:
-        return capnp.TwoPartyClient(f"{host}:{port}").bootstrap().cast_as(simulator)
+        reader, writer = await asyncio.open_connection(host, port)
+        client = capnp.TwoPartyClient()
+        asyncio.gather(
+            capnpreader(client, reader),
+            capnpwriter(client, writer),
+            return_exceptions=True)
+        return client.bootstrap().cast_as(simulator)
     except:# ConnectionRefusedError: inside docker weird stuff happens
         # if we're doing a local simulation, start a new server
-        if host=="localhost":
+        if host=="localhost" and autostart:
             print("starting new local server")
             if simulator == Ngspice:
                 simcmd = "NgspiceSimServer"
@@ -33,7 +59,7 @@ def connect(host, port=5923, simulator=Ngspice):
 
             Popen([simcmd, str(port)])
             sleep(1) # wait a bit for the server to start :(
-            return connect(host, port, simulator)
+            return await connect(host, port, simulator, False)
         else:
             raise
 
@@ -63,12 +89,12 @@ def map_complex(vec):
     return np.fromiter((complex(v.real, v.imag) for v in vec), complex)
 
 
-def read(response):
+async def read(response):
     """
     Read one chunk from a simulation command
     """
     data = {}
-    res = response.result.read().wait()
+    res = await response.result.read().a_wait()
     # print(res)
     for vecs in res.data:
         # this set of vectors is not initialised, skip it
@@ -92,35 +118,35 @@ def read(response):
     return res.more, data
 
 
-def stream(response, streamdict):
+async def stream(response, streamdict):
     """
     Stream simulation data into a Buffer (DataFrame)
     """
     more = True
     while more:
-        more, res = read(response)
+        more, res = await read(response)
         for k, v in res.items():
             if k in streamdict and list(v.columns) == list(streamdict[k].data.columns):
                 streamdict[k].send(v)
             else:
                 buf = Buffer(v, length=int(1e9), index=False)
                 streamdict[k] = buf
-        yield
 
 
-def readAll(response):
+async def readAll(response):
     """
     Read all the simulation data from a simulation command.
     """
-    cdsdict = {}
-    for _ in stream(response, cdsdict):
-        pass
-    return cdsdict
+    streamdict = {}
+    await stream(response, streamdict)
+    return streamdict
 
-if __name__ == "__main__":
-    con = connect("localhost")
+async def main():
+    con = await connect("localhost")
     fs = loadFiles(con, "test.cir")
     res = fs.commands.tran(1e-6, 1e-3, 0)
     d = {}
-    for _ in stream(res, d):
-        print(d)
+    print(await readAll(res))
+
+if __name__ == "__main__":
+    asyncio.run(main())
